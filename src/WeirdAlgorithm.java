fb092d9f42efd441b974a75ed3b2cb85c771d2d1
original-codematrix <jan@codematrix.dev>
2020-06-27 11:45:28 +0200

    Reformat project

--- src/WeirdAlgorithm.java
+++ src/WeirdAlgorithm.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public final class WeirdAlgorithm {
 
-	public static void main(final String[] args) {
-		final Scanner scanner = new Scanner(System.in);
-		long i = scanner.nextLong();
-		System.out.print(i);
-		while(i > 1) {
-			if(i % 2 == 0) {
-				i /= 2;
-			} else {
-				i = 3 * i +1;
-			}
-			System.out.print(" " + i);
-		}
-	}
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        long i = scanner.nextLong();
+        System.out.print(i);
+        while (i > 1) {
+            if (i % 2 == 0) {
+                i /= 2;
+            } else {
+                i = 3 * i + 1;
+            }
+            System.out.print(" " + i);
+        }
+    }
 }
